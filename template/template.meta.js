c81ba085e083c074cb6c68043968805ffa6642ed
jameshsu1125 <jameshsu1125@gmail.com>
2023-06-30 20:35:37 +0800

    adjustment meta

--- template/template.meta.js
+++ template/template.meta.js
@@ -1,5 +1,6 @@
 module.exports = {
-	title: '',
-	description: '',
+	title: '台灣奧美-Pride Month',
+	description:
+		'在台灣奧美，Ogilvy #Pride2023不僅是一種價值觀的展示，更有別於其他辦公室的是，我們用2支影片真真實實地參與了『婚姻平權法』這場運動，甚至扮演了推波助瀾的角色。',
 	url: 'https://jameshsu1125.github.io/',
 };
